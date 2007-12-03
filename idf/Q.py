7b5a79c138c7806e0c9b968e29c57acdec8f2d18
Max Kresch <mkresch.caltech@gmail.com>
2007-12-03 20:28:50 +0000

    Added a read write for Omega2.v1.  Also touched up Q.py.

--- idf/Q.py
+++ idf/Q.py
@@ -10,7 +10,7 @@ dubSize = calcsize('<d')
 strSize = calcsize('<s')
 
 def write(Q,filename='Q',comment=''):
-  """Takes numpy Q in with shape (numQ,Dim) and writes to binary file."""
+  """Takes numpy Q in with shape (N_q,D) and writes to binary file."""
   f=open(filename,'w')
   f.write(pack('<64s','Q'))
   f.write(pack('<i',version))
@@ -25,12 +25,12 @@ def read(filename='Q'):
   """Takes filename, returns a tuple with information and Q as a numpy."""
   f=open(filename,'r').read()
   i = 0
-  filetype, = unpack('<64s',f[i:i+64*strSize])          ; i += 64*strSize
-  version,  = unpack('<i',f[i:i+intSize])               ; i += intSize
-  comment,  = unpack('<1024s',f[i:i+1024*strSize])      ; i += 1024*strSize
-  Dim,numQ  = unpack('<2i',f[i:i+2*intSize])            ; i += 2*intSize
-  Q         = unpack('<%id' % (numQ*Dim),f[i:])
+  filetype,= unpack('<64s',f[i:i+64*strSize])          ; i += 64*strSize
+  version, = unpack('<i',f[i:i+intSize])               ; i += intSize
+  comment, = unpack('<1024s',f[i:i+1024*strSize])      ; i += 1024*strSize
+  D,N_q    = unpack('<2i',f[i:i+2*intSize])            ; i += 2*intSize
+  Q        = unpack('<%id' % (N_q*D),f[i:])
   Q = numpy.array(Q)
-  Q.shape = (numQ,Dim)
+  Q.shape = (N_q,D)
   return (filetype.strip('\x00'),version,comment.strip('\x00')),Q
 
