65571281df99d33d72e18368930d0e1eda67f9bf
"J. Brandon Keith" <jbrkeith@gmail.com>
2007-12-13 21:39:56 +0000

    a few more changes to polarizationIO

--- idf/PolarizationIO.py
+++ idf/PolarizationIO.py
@@ -11,27 +11,28 @@ strSize = calcsize('<s')
 
 class PolarizationIO:
     
-    def __init__(self,filename='Polarizations',comment=''):
+    def __init__(self, filename='Polarizations', comment='',
+                 dimension=3, numAtoms=1, numks=1):
         self.f=open(filename,'w')
         self.f.write(pack('<64s','Polarizations'))
         self.f.write(pack('<i',version))
         self.f.write(pack('<1024s',comment))
+        self.f.write(pack('<i',dimension))
+        self.f.write(pack('<i',numAtoms))
+        self.f.write(pack('<i',numks))
 
     def writeVec(self,vec):
-        """Takes numpy Polarizations with shape (N_q,N_b*D,N_b,D) and writes \n
+        """Takes numpy Polarizations with shape (N_b,D) and writes \n
 to binary file."""
     
-        res = numpy.zeros( Pols.shape + (2,) )
-        res[:,:,:,:,0] = numpy.real(Pols)
-        res[:,:,:,:,1] = numpy.imag(Pols)
-        self.f.write(pack('<i',res.shape[3]))
-        self.f.write(pack('<i',res.shape[2]))
-        self.f.write(pack('<i',res.shape[0]))
+        res = numpy.zeros( vec.shape + (2,) )
+        res[:,:,0] = numpy.real(vec)
+        res[:,:,1] = numpy.imag(vec)
         res = tuple( res.reshape( (-1) ) )
         self.f.write( pack('<%id' % len(res),*res) )
         return
 
-    def read(filename='Polarizations'):
+    def read(self,filename='Polarizations'):
         """Takes filename, returns a tuple with information and Polarizations \n
 as a numpy."""
         f=open(filename,'r').read()
