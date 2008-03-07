cb33b415b6879a24732369c57601e52f03c1e66a
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-03-07 19:13:28 +0000

    adding make.mm support and rearranging

---
+++ translator/translator/Make.mm
@@ -0,0 +1,47 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = translator
+
+#--------------------------------------------------------------------------
+#
+
+BUILD_DIRS = \
+
+	
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+
+#--------------------------------------------------------------------------
+#
+# export
+
+EXPORT_PYTHON_MODULES = \
+    __init__.py \
+    hisToNc.py \
+    GeneralSettings.py \
+
+
+export:: export-package-python-modules
+
+# version
+# $Id$
+
+# End of file
