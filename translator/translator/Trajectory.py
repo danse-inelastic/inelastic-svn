2ab63f83f119334492dff923c80138d3534741ae
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-03-07 19:02:13 +0000

    adding pyre wrapper to translator

---
+++ translator/translator/Trajectory.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                              Brandon Keith
+#                      California Institute of Technology
+#              (C) 2007 All Rights Reserved  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+from pyre.components.Component import Component
+from pyregui.inventory.extensions.OutputDir import OutputDir
+from pyregui.inventory.extensions.InputFile import InputFile
+
+class Trajectory(Component):
+    '''given input parameters and coordinates of a trajectory, returns the coherent
+    scattering function'''
+    
+    class Inventory(Component.Inventory):   
+        import pyre.inventory as inv 
+        speciesNumbers = inv.str( "List of (specie, specie count)", default = "")
+        speciesNumbers.meta['tip'] = "list of tuples such as ('H',40)"
+        speciesNumbers.meta['importance'] = 10
+        historyFile = InputFile( ".his Filename", default = "")
+        historyFile.meta['tip'] = "the trajectory file of the MD run (NetCDF format)"
+        historyFile.meta['importance'] = 9
+
+    def __init__(self, name = 'Trajectory', facility=None):
+        Component.__init__(self, name, facility)
+        self.i=self.inventory
+        
+    def translate(self):
+        from hisToNc import hisToNc
+        hisToNc(self.i.speciesNumbers,self.i.historyFile,blockSize=1)
+        
+        
+    
+    
+    
+
+    
