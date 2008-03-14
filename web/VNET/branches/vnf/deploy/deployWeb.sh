fd23e6d81cc2170547085522e8b0be1a85a95b32
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-03-14 07:14:50 +0000

    adding more ent examples and tables examples; lots to do still!

--- web/VNET/branches/vnf/deploy/deployWeb.sh
+++ web/VNET/branches/vnf/deploy/deployWeb.sh
@@ -7,7 +7,8 @@ cp -r ../html/* /var/www/apache2-default
 cp -r ../content $cgi
 cp -r ../config $cgi
 cp -r ../sampleassemblies $cgi
+cp -r ../cgi $cgi
 
-cp ../cgi/main.py $cgi
-cp ../cgi/main.sh $cgi
-cp ../cgi/dottools $cgi
+#cp ../cgi/main.py $cgi
+#cp ../cgi/main.sh $cgi
+#cp ../cgi/dottools $cgi
