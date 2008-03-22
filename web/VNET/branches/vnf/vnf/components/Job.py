f0004c9b85693bb09529dcb8f60af6b82f0c425c
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-03-22 22:31:55 +0000

    committing files I forgot to add in previous commit

---
+++ web/VNET/branches/vnf/vnf/components/Job.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                  Jiao Lin
+#                     California Institute of Technology
+#                       (C) 2007  All Rights Reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+from Actor import Actor, action_link, action, actionRequireAuthentication
+from wording import plural, present_be
+
+
+class Job(Actor):
+
+
+    class Inventory(Actor.Inventory):
+
+        import time
+        import pyre.inventory
+
+        id = pyre.inventory.str("id", default=None)
+        id.meta['tip'] = "the unique identifier for a given search"
+        
+        page = pyre.inventory.str('page', default='empty')
+
+        pass # end of Inventory
+
+
+
+    def default(self, director):
+        return self.listall( director )
+
+
+    def listall(self, director):
+        page = director.retrievePage( 'job' )
+        
+        main = page._body._content._main
+        
+        # populate the main column
+        document = main.document(title='List of computational jobs')
+        document.description = ''
+        document.byline = 'byline?'
+
+        # retrieve id:record dictionary from db
+        clerk = director.clerk
+        sampleassemblies = clerk.indexSampleAssemblies()
+        
+        listsampleassemblies( sampleassemblies.values(), document, director )
+        
+        return page
+
+
+    def edit(self, director):
+        try:
+            page = director.retrieveSecurePage( 'sampleassembly' )
+        except AuthenticationError, error:
+            return error.page
+        
+        main = page._body._content._main
+
+        # the record we are working on
+        id = self.inventory.id
+        sampleassembly = self._getsampleassembly( id, director )
+
+        # populate the main column
+        document = main.document(title='Sample Assembly: %s' % sampleassembly.short_description )
+        document.description = (
+            'Sample assembly is a collection of neutron scatterers. For example, '\
+            'it can consist of a main sample, a sample container, and a furnace.\n'\
+            )
+        document.byline = 'byline?'
+
+        scatterers = self._getscatterers( id, director )
+
+        if len(scatterers) == 0:
+            noscatterer( document, director )
+        else:
+            listscatterers( scatterers, document, director )
+            pass
+    
+        return page    
+
+
+    def __init__(self, name=None):
+        if name is None:
+            name = "sampleassembly"
+        super(SampleAssembly, self).__init__(name)
+        return
+
+
+    def _getsampleassembly(self, id, director):
+        clerk = director.clerk
+        return clerk.getSampleAssembly( id )
+
+
+    def _getscatterers(self, id, director):
+        clerk = director.clerk
+        return clerk.getScatterers( id )
+
+
+    pass # end of SampleAssembly
+
+
+
+
+def listscatterers( scatterers, document, director ):
+    p = document.paragraph()
+
+    n = len(scatterers)
+    p.text = [ 'There %s %s scatterer%s in this sample assembly: ' %
+               (present_be(n), n, plural(n))
+                ]
+
+    from inventorylist import list
+    list( scatterers, document, 'scatterer', director )
+    return
+
+
+def listsampleassemblies( sampleassemblies, document, director ):
+    p = document.paragraph()
+
+    n = len(sampleassemblies)
+
+    p.text = [ 'There %s %s sampleassembl%s: ' %
+               (present_be(n), n, plural(n, 'y'))
+                ]
+
+    from inventorylist import list
+    list( sampleassemblies, document, 'sampleassembly', director )
+    return
+
+
+
+def noscatterer( document, director ):
+    p = document.paragraph()
+
+    link = action_link(
+        actionRequireAuthentication(
+        'scatterer',
+        director.sentry,
+        label = 'add',
+        routine = 'new',
+        ),  director.cgihome
+        )
+    
+    p.text = [
+        "There is no scatterer in this sample assembly. ",
+        'Please %s a scatter.' % (
+        director.cgihome, link)
+        ]
+    return
+
+
+# version
+__id__ = "$Id$"
+
+# End of file 
