45575275608ba37fffa77277380e67a3e5fba520
Jiao <yxqd@users.noreply.github.com>
2008-04-17 16:21:26 +0000

    Job actor refactored: job editing form moved to vnf/content/forms

--- web/VNET/branches/vnf/vnf/components/Job.py
+++ web/VNET/branches/vnf/vnf/components/Job.py
@@ -27,14 +27,6 @@ class Job(base):
         id = pyre.inventory.str("id", default=None)
         id.meta['tip'] = "the unique identifier for a given search"
 
-        name = pyre.inventory.str( 'name', default = 'jobname' )
-
-        server = pyre.inventory.str( 'server', default = 'serverid' )
-
-        numprocessors = pyre.inventory.int( 'numprocessors', default = 1 )
-        
-        page = pyre.inventory.str('page', default='empty')
-
 
     def default(self, director):
         return self.listall( director )
@@ -83,60 +75,29 @@ class Job(base):
         
         main = page._body._content._main
 
-        id = self.processFormInputs( director )
+        job = self.processFormInputs( director )
         
         document = main.document( title = 'Job editor' )
 
-        job = director.clerk.getJob( id )
+        formcomponent = self.retrieveFormToShow( 'job' )
+        formcomponent.inventory.id = job.id
+        formcomponent.director = director
 
-        objtype = job.__class__
-        objtypename = objtype.__name__
-        
         form = document.form(
-            name=objtypename,
-            legend='Job #%s' % job.id,
+            name='job',
+            legend = formcomponent.legend(),
             action=director.cgihome)
 
-        actor = 'job'
-        actor_field = form.hidden(name='actor', value=actor)
-        routine_field = form.hidden(name='routine', value='submit')
-        id_field = form.hidden(
-            name = '%s.id' % actor, value = id)
-        
-        username_filed = form.hidden(
-            name='sentry.username', value = director.sentry.username)
-        ticket_filed = form.hidden(
-            name='sentry.ticket', value = director.sentry.ticket)
+        # specify action
+        action = actionRequireAuthentication(
+            actor = 'job', sentry = director.sentry,
+            label = '', routine = 'submit',
+            arguments = {'form-received': formcomponent.name } )
+        from vnf.weaver import action_formfields
+        action_formfields( action, form )
 
-        # properties of a job
-        field = form.text(
-            id = 'name',
-            name = '%s.name' % actor,
-            label = 'name',
-            value = job.jobName )
-
-        servers = director.clerk.getServers()
-        entries = [ (server.id, server.server) for server in servers ]
-        ids = [ id for id, server in entries ]
-
-        selected_server = job.server
-        if selected_server not in ids: selected_server = entries[0][0]
-        
-        import opal.content
-        selector = opal.content.selector(
-            name = '%s.server' % actor,
-            entries = entries,
-            label = 'server',
-            selected = selected_server,
-            )
-        form.contents.append( selector )
-
-        field = form.text(
-            id = 'numprocessors',
-            name = '%s.numprocessors' % actor,
-            label = 'number of processors',
-            value = job.numprocessors,
-            )
+        # expand the form with fields of the data object that is being edited
+        formcomponent.expand( form )
 
         submit = form.control(name="submit", type="submit", value="Submit")
         
@@ -151,19 +112,10 @@ class Job(base):
         
         main = page._body._content._main
 
-        id = self.inventory.id
-        name = self.inventory.name
-        server = self.inventory.server
-        numprocessors = self.inventory.numprocessors
+        job = self.processFormInputs( director )
 
-        job = director.clerk.getJob( id )
-        job.jobName = name
-        job.server = server
-        job.numprocessors = numprocessors
-
-        director.clerk.updateRecord( job )
-        
-        server_record = director.clerk.getServer( server )        
+        server = job.server
+        server_record = director.clerk.getServer( server )
 
         try:
             schedule(job, director)
