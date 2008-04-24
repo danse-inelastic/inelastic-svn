a5a3060fc700a70d97f4eeac39adfec57a5a36f3
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-04-24 17:42:35 +0000

    organizing the supporting calculations page

--- web/VNET/branches/vnf/vnf/components/SupportingCalcs.py
+++ web/VNET/branches/vnf/vnf/components/SupportingCalcs.py
@@ -26,6 +26,38 @@ class SupportingCalcs(Actor):
     def default(self, director):
         page = director.retrievePage('supportingCalcs')
         main = page._body._content._main
+        
+        # populate the main column
+        document = main.document(title='Supporting Calculations')
+    
+        # build the sample input form
+        p = document.paragraph()
+        p.text = ['Simulate the material with atomistic forcefields (powered by GULP/MMTK)',
+                  '<a href="GULP.jnlp">Gulp</a>']
+        
+        atoms = document.form(name='login', legend='Upload xyz file', action=app.cgihome)
+        
+        name = atoms.text(id='name', name='name', label='Sample Name')
+        name.help = 'An identifying name for this sample.'
+        
+        atomFile = atoms.file(id='atomFile', name='atomFile', label='Xyz file containing atoms')
+        atomFile.help = 'Lattice vectors a,b,c should be on comment line in form a_x a_y a_z b_x b_y b_z c_x c_y c_z'
+        
+        submit = atoms.control(name="submit", type="submit", value="submit")
+        
+        # view the sample (maybe put this on another page after clicking
+        # to submit the sample
+#        p = document.paragraph()
+#        p.text = ['''<script type="text/javascript"> 
+#    jmolInitialize("jmol-11.4.RC10"); 
+#    jmolApplet(600, "load orthoGraphKH212x12x1.xyz");</script>''']
+#        p = document.paragraph()
+#        p.text = [
+#            '''<strong>Note</strong>: would like to modify material and/or construct overall sample shape here.  
+#    It may be advisable to reuse DANSE Python code by Python/Jython 
+#    Web Start (http://personalpages.tds.net/~kent37/Python/JythonWebStart.html)'''
+#            ]
+
         return page 
 
 
