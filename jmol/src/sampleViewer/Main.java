24f6592706ee21008b812857940ba30a2aae6c3c
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-05-17 00:57:03 +0000

    starting quick wrapper to load samples for visualization

---
+++ jmol/src/sampleViewer/Main.java
@@ -0,0 +1,17 @@
+package sampleViewer;
+
+import javax.swing.JFrame;
+
+import org.openscience.jmol.app.Jmol;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Jmol jmol = Jmol.getJmol(new JFrame(), 640, 480, "");
+
+	}
+
+}
