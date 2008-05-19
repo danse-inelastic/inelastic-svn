89128a26dd31d0da6b5dd715f8a546d607ca538f
"J. Brandon Keith" <jbrkeith@gmail.com>
2008-05-19 11:22:45 +0000

    finally got the sample Viewer to work!  Now just need to make it download from a URL instead of from the local file system...

--- jmol/src/sampleViewer/Main.java
+++ jmol/src/sampleViewer/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		Jmol jmol = Jmol.getJmol(new JFrame(), 640, 480, "");
 		//File temp = new File(System.getProperty("user.home") + "/" + System.nanoTime() + ".xyz");
-		File temp = new File(System.getProperty("user.dir") + "/sample.cif");
+		File temp = new File(System.getProperty("user.dir") + "/"+args[0]);
 		jmol.viewer.openFile(temp.getPath());
 
 	}
