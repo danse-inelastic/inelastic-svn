329d8df00632bd5ae611b5f31531d48a4c1327d5
Jiao <yxqd@users.noreply.github.com>
2008-07-08 10:23:02 +0000

    tabulator/trunk/tests/make-test-table-2.js: minor. make example table more random.

--- web/js/tabulator/trunk/tests/make-test-table-2.js
+++ web/js/tabulator/trunk/tests/make-test-table-2.js
@@ -31,16 +31,66 @@ function make_test_table_2( thetable ) {
       { name: 'Bestseller', klass: 'single_choice_in_one_column'}
       ) ;
 
-  var N = 50;
+  var N = 30;
+  var BestSeller = Math.floor( Math.random() * N );
 
   for (var i = 0; i < N; i++ ) {
     
     var sales = Math.floor( Math.random() * 100 ) - Math.floor(100/2);
     var price = Math.floor( Math.random() * 3000 )/100.;
     var shipping = Math.floor( Math.random() * 6 );
-    
-    thetable.table_addrow_dataonly( sales, 'a book', "an author", price, 1, shipping, 0 );
+    var title = randSentence( 3, 10 );
+    var author = randSentence( 2, 8 );
+    var instore = Math.floor( Math.random() * 2 );
+    thetable.table_addrow_dataonly
+      ( sales, title, author, price, instore, shipping, i==BestSeller );
     ;
   }
 
+}
+
+
+function randSentence(  maxnumberofwords, maxnumberofcharactersperword )
+{
+  var words = randWords(  maxnumberofwords, maxnumberofcharactersperword );
+
+  var sentence = "";
+
+  for ( var i=0; i<words.length; i++ ) {
+    sentence += words[i];
+    if (i<words.length-1) sentence += ' ';
+  }
+  
+  return sentence;
+}
+
+
+function randWords( maxnumberofwords, maxnumberofcharactersperword )
+{
+  var nword = Math.floor( Math.random() * (maxnumberofwords-1) ) + 1;
+  var words = [];
+
+  for (var i=0; i<nword; i++) {
+    words.push( randWord( maxnumberofcharactersperword ) );
+  }
+  
+  return words;
+}
+
+
+function randWord( maxlength ) {
+  var characters = "0123456789"
+    + "ABCDEFGHIJKLMNOPQRSTUVWXTZ"
+    + "abcdefghiklmnopqrstuvwxyz";
+  
+  var length = Math.floor( Math.random() * (maxlength-1) ) + 1;
+
+  var word="";
+
+  for (var j=0;j<length;j++) {
+    var rand = Math.floor(Math.random() * characters.length);
+    word += characters.substring(rand,rand+1);
+  }
+
+  return word;
 }
