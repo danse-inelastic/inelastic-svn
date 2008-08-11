887ddd2df9bb842ef865325e7d7230a67d16295e
Jiao <yxqd@users.noreply.github.com>
2008-08-11 09:13:54 +0000

    js/slidableGallery: clean up a bit

--- web/js/slidableGallery/trunk/js/slidableGallery.js
+++ web/js/slidableGallery/trunk/js/slidableGallery.js
@@ -41,9 +41,20 @@
   //                 <span class="slider-lbl4">Applications</span>
   //                 <span class="slider-lbl5">Servers</span>
   //             </div>
-  $.fn.slidableGallery = function () {
+  $.fn.slidableGallery = function ( slider_labels ) {
 
+    // dimensions
+    // !! these cannot be changed... 
+    // The reason is the images for the slider has fixed widths...
+    width = 560 // width of widget
+    sliderwidth = 542;
+
+    // 
+    sliderheight = 17; // should be adjustable?
+    spacer = 3;  // space between ul and slider
+	
     container = this;
+    container.width( width );
 
     // everytime an item loaded, check whether all items are loaded
     // if all items are loaded, emit "slider-images-all-loaded"
@@ -63,13 +74,38 @@
     container.bind( 'slider-images-all-loaded', function() {
 	
 	// debug
-	console.log( 'debug' );
-	
+	// console.log( 'debug' );
+
+
+	// calculate the total length of images
 	var container = $(this); 
 	var ul = $('ul', container);
-            
 	var itemsWidth = ul.innerWidth() - container.outerWidth();
-            
+	var ulheight = ul.height();
+
+	container.height( ulheight + sliderheight + spacer );
+	
+	// add slider and labels
+	sliderdiv = $( '<div class="slider"/>' );
+	sliderdiv.width( sliderwidth );
+	sliderdiv.height( sliderheight );
+	sliderdiv.css( 'margin-top', ulheight + spacer );
+
+	sliderhandle = $( '<div class="handle"/>' ) ;
+	sliderhandle.height( sliderheight );
+	//sliderhandle.width( sliderwidth*1.0/itemsWidth*sliderwidth );
+	sliderdiv.append( sliderhandle );
+
+	for (i=0; i<slider_labels.length; i++) {
+	  label = slider_labels[i];
+	  position = label.position;
+	  text = label.text;
+	  labelhtml = $('<span>'+text+'</span>');
+	  labelhtml.css( 'left', sliderwidth * position + 'px' );
+	  sliderdiv.append( labelhtml );
+	}
+	container.append( sliderdiv );
+
 	$('.slider', container).slider( {
 	    min: 0,
 	    max: itemsWidth,
--- web/js/slidableGallery/trunk/tests/apple-products.js
+++ web/js/slidableGallery/trunk/tests/apple-products.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function() {
 
-    $('div.slidableGallery').slidableGallery();
+    labels = [ {'text': 'Wi-Fi', 'position': 0.2},
+	       {'text': 'Macs', 'position': 0.4},
+	       {'text': 'Applications', 'position': 0.6},
+	       {'text': 'Servers', 'position': 0.8}
+	       ];
+    $('div.slidableGallery').slidableGallery( labels );
 
   });
